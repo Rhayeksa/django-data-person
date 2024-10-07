293bb8eaed17540e384f6f76a1c4a161ae707f3f
rhayeksa <rhayeksads@gmail.com>
2024-10-07 10:41:24 +0700

    regis url web and config image upload

--- core/urls.py
+++ core/urls.py
@@ -14,9 +14,15 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf.urls.static import static
 from django.contrib import admin
-from django.urls import path
+from django.urls import include, path
+
+from .settings import MEDIA_ROOT, MEDIA_URL
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path(route="", view=include(arg="apps.web.urls")),
 ]
+
+urlpatterns += static(prefix=MEDIA_URL, document_root=MEDIA_ROOT)
